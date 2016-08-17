2e15e023bc3cf07d33ed86b274699558a8e084c6
Kira McLean <kiraemclean@gmail.com>
2016-08-17 13:04:47 -0400

    sort by created at

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects
   def index
-    @projects = Project.all
+    @projects = Project.all.order(created_at: :desc)
 
     render json: @projects
   end
